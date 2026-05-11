3dcac11243ceb56113c7253968881514bc82fd85
LareinaWeii <122090567@link.cuhk.edu.cn>
2026-05-11 21:11:55 +0800

    initial commit

---
+++ launch/display.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    package_name = 'Arm_urdf'
+
+    pkg_path = get_package_share_directory(package_name)
+    urdf_path = os.path.join(pkg_path, 'urdf', 'Arm_urdf.urdf')
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            arguments=[urdf_path],
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+        )
+    ])
